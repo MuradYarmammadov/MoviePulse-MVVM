eb09899cbdd9bb36388016d3a4a4757b30e7e085
Murad Yarmammadov <yarmamedov.murad01@gmail.com>
2023-12-06 15:10:12 +0100

    Initial Commit

---
+++ MoviePulse MVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoviePulse MVVM
+//
+//  Created by Murad Yarmamedov on 06.12.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
